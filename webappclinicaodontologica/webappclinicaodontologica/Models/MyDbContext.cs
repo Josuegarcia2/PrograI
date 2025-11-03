761d0356ae28b52ce94fee2fb2659a9f19e95cf5
JOSUÉ ELÍAS GARCÍA LÓPEZ <USSS046123@ugb.edu.sv>
2025-11-03 17:36:10 -0600

    Creacion de la rama webapp y del proyecto web app clinica odontologica, configuracion de la clase MyDbContext y usuario y la configuracion de la cadena de conexion a la BD

---
+++ webappclinicaodontologica/webappclinicaodontologica/Models/MyDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace webappclinicaodontologica.Models
+{
+    public class MyDbContext: DbContext
+    {
+        public MyDbContext() { }
+
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options) { }
+            public DbSet<usuario> usuarios { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<usuario>().HasKey(u => u.id_usuario);
+        }
+    }
+}
---
+++ webappclinicaodontologica/webappclinicaodontologica/Models/usuario.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace webappclinicaodontologica.Models
+{
+    public class usuario
+    {
+        [Key]
+        public int id_usuario { get; set; }
+        public string usuario_nombre { get; set; }
+        public string contrasena { get; set; }
+        public int id_rol { get; set; }
+    }
+}
