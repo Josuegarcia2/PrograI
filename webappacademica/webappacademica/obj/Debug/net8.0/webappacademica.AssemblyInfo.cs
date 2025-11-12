219988284f1c6fe40097845b5e7b43faa891dfa9
JOSUÉ ELÍAS GARCÍA LÓPEZ <USSS046123@ugb.edu.sv>
2025-11-12 01:10:07 -0600

    ln guarda al docente

--- webappacademica/webappacademica/obj/Debug/net8.0/webappacademica.AssemblyInfo.cs
+++ webappacademica/webappacademica/obj/Debug/net8.0/webappacademica.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("webappacademica")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+382b2850c51b75cb79a7131fdcc31af34d8d22c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69c444413fc25a2edf681911197ce9b93321091d")]
 [assembly: System.Reflection.AssemblyProductAttribute("webappacademica")]
 [assembly: System.Reflection.AssemblyTitleAttribute("webappacademica")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
